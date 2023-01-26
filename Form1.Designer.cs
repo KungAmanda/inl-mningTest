c6be5e4b6a7d9ac33900cded7f0e508aca5031bf
kungamanda <amanda-leia.azmoudeh@student.nbi-handelsakademin.se>
2023-01-26 18:03:23 +0100

    version 2.0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,19 +32,22 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.filmBox1 = new System.Windows.Forms.TextBox();
+            this.regiBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.genreBox2 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.ratingBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -88,19 +91,19 @@
             this.label4.Text = "Rating 1-10 :";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // textBox1
+            // filmBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(51, 57);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(191, 20);
-            this.textBox1.TabIndex = 4;
+            this.filmBox1.Location = new System.Drawing.Point(51, 57);
+            this.filmBox1.Name = "filmBox1";
+            this.filmBox1.Size = new System.Drawing.Size(191, 20);
+            this.filmBox1.TabIndex = 4;
             // 
-            // textBox2
+            // regiBox3
             // 
-            this.textBox2.Location = new System.Drawing.Point(78, 114);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(164, 20);
-            this.textBox2.TabIndex = 5;
+            this.regiBox3.Location = new System.Drawing.Point(78, 114);
+            this.regiBox3.Name = "regiBox3";
+            this.regiBox3.Size = new System.Drawing.Size(164, 20);
+            this.regiBox3.TabIndex = 5;
             // 
             // button1
             // 
@@ -111,6 +114,7 @@
             this.button1.TabIndex = 7;
             this.button1.Text = "spara";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label5
             // 
@@ -122,12 +126,12 @@
             this.label5.TabIndex = 8;
             this.label5.Text = "Genre:";
             // 
-            // textBox4
+            // genreBox2
             // 
-            this.textBox4.Location = new System.Drawing.Point(63, 88);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(179, 20);
-            this.textBox4.TabIndex = 9;
+            this.genreBox2.Location = new System.Drawing.Point(63, 88);
+            this.genreBox2.Name = "genreBox2";
+            this.genreBox2.Size = new System.Drawing.Size(179, 20);
+            this.genreBox2.TabIndex = 9;
             // 
             // dataGridView1
             // 
@@ -136,38 +140,32 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(476, 181);
             this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(305, 433);
+            this.button2.Location = new System.Drawing.Point(300, 425);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(79, 25);
+            this.button2.Size = new System.Drawing.Size(79, 24);
             this.button2.TabIndex = 11;
             this.button2.Text = "Ändra";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(390, 432);
+            this.button3.Location = new System.Drawing.Point(385, 426);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(84, 25);
+            this.button3.Size = new System.Drawing.Size(84, 24);
             this.button3.TabIndex = 12;
             this.button3.Text = "Ta bort";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(104, 146);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(138, 21);
-            this.comboBox1.TabIndex = 13;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(9, 439);
+            this.label6.Location = new System.Drawing.Point(4, 431);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(38, 19);
             this.label6.TabIndex = 14;
@@ -175,20 +173,46 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(51, 437);
+            this.textBox3.Location = new System.Drawing.Point(46, 429);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(167, 20);
             this.textBox3.TabIndex = 15;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(224, 434);
+            this.button4.Location = new System.Drawing.Point(348, 182);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 24);
+            this.button4.Size = new System.Drawing.Size(136, 28);
             this.button4.TabIndex = 16;
-            this.button4.Text = "Sök";
+            this.button4.Text = "Hämta inlagda filmer";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(8, 464);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(476, 171);
+            this.dataGridView2.TabIndex = 17;
+            // 
+            // ratingBox4
+            // 
+            this.ratingBox4.Location = new System.Drawing.Point(105, 144);
+            this.ratingBox4.Name = "ratingBox4";
+            this.ratingBox4.Size = new System.Drawing.Size(137, 20);
+            this.ratingBox4.TabIndex = 18;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(219, 426);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 24);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Sök";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
@@ -196,18 +220,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(990, 647);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.ratingBox4);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.genreBox2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.regiBox3);
+            this.Controls.Add(this.filmBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -216,6 +242,7 @@
             this.Text = "Fake";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,18 +254,20 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox filmBox1;
+        private System.Windows.Forms.TextBox regiBox3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox genreBox2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox ratingBox4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace inlämningTest
 {
@@ -15,6 +17,7 @@ namespace inlämningTest
     {
         // gör så att den kan kontakta databasen 
         MySqlConnection conn;
+        private string connectionString;
 
         public Form1()
         {
@@ -29,9 +32,37 @@ namespace inlämningTest
             string connString = $"SERVER={server};DATABASE={database};UID={user};PASSWORD={password};";
 
             conn = new MySqlConnection(connString);
-
-
         }
+
+       /* private bool ValidateTextBoxes()
+        {
+             bool isValid = true;
+
+            //loop genom alla textboxar i formuläret
+            foreach (Control control in this.Controls)
+            {
+                if (control is TextBox)
+                {
+                    TextBox textBox = control as TextBox;
+                    if (string.IsNullOrWhiteSpace(textBox.Text))
+                    {
+                        textBox.BackColor = Color.IndianRed;
+                        isValid = false;
+                    }
+                    else
+                    {
+                        textBox.BackColor = SystemColors.Window;
+                    }
+                }
+            }
+
+            if (!isValid)
+            {
+                MessageBox.Show("Please fill in all the fields.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return isValid;
+        }*/
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -44,7 +75,170 @@ namespace inlämningTest
         }
 
         private void button4_Click(object sender, EventArgs e)
+        {    
+            // Gör en ny koppling 
+            string connectionString = "Server=localhost;Port=3306;Database=databoos;Uid=root;Pwd=Simon123;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM film", connection);
+
+            // Så att datan kan visa sig på griden när man klickar på sök knappen
+            try
+            {
+                connection.Open();
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
+            // Validera att alla textboxar är fyllda
+            if (string.IsNullOrWhiteSpace(filmBox1.Text) || string.IsNullOrWhiteSpace(genreBox2.Text) || 
+                string.IsNullOrWhiteSpace(regiBox3.Text) || string.IsNullOrWhiteSpace(ratingBox4.Text))
+            {
+                MessageBox.Show("Alla fält måste vara ifyllda!");
+                return;
+            }
+
+            // Hämta värden från textboxarna
+            string filmBox1Value = filmBox1.Text;
+            string genreBox4Value = genreBox2.Text;
+            string regiBox2Value = regiBox3.Text;
+            string ratingBox5Value = ratingBox4.Text;
+
+            // Skapa en anslutning till MySQL Workbench
+            string connectionString = "Server=localhost;Database=databoos;Uid=root;Pwd=Simon123;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                // Öppna anslutningen
+                connection.Open();
+
+                // Skapa och exekvera SQL-fråga för att lägga till data i databasen
+                string query = $"INSERT INTO film (Film, Regissör, Genre, Rating) VALUES ('{filmBox1Value}', '{regiBox2Value}', '{genreBox4Value}', '{ratingBox5Value}')";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+
+                // Visa bekräftelsemeddelande
+                MessageBox.Show("Filmen har nu sparats! Du kan klicka på 'hämta inlagda filmer' knappen för att se den in tabellen!");
+            }
+            catch (Exception ex)
+            {
+                // Visa felmeddelande
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                // Stäng anslutningen
+                connection.Close();
+            }
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            // Ange information för att ansluta till databasen
+            string connectionString = "Server=localhost;Port=3306;Database=databoos;Uid=root;Pwd=Simon123;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd;
+            string searchText = textBox3.Text; // Spara det som skrivs i textBox3 i en variabel
+
+            // Kontrollera vilken tabell som ska sökas i beroende på innehållet i textBox3
+            if (searchText.Contains("film"))
+            {
+                cmd = new MySqlCommand("SELECT * FROM film WHERE Film LIKE '%" + searchText + "%'", connection);
+            }
+            else if (searchText.Contains("genre"))
+            {
+                cmd = new MySqlCommand("SELECT * FROM genre WHERE Genre LIKE '%" + searchText + "%'", connection);
+            }
+            else if (searchText.Contains("reggisör"))
+            {
+                cmd = new MySqlCommand("SELECT * FROM regissör WHERE Regissör LIKE '%" + searchText + "%'", connection);
+            }
+            else
+            {
+                cmd = new MySqlCommand("SELECT * FROM film WHERE Film LIKE '%" + searchText + "%' OR Genre LIKE '%" + searchText + "%' OR Regissör LIKE '%" + searchText + "%'", connection);
+            }
+
+            try
+            {
+                connection.Open();
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt; // Visa resultatet i dataGridView1
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string connectionString = "Server=localhost;Port=3306;Database=databoos;Uid=root;Pwd=Simon123;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            // Skapar en SQL-fråga för att uppdatera en rad i tabellen 'film' med de värden som finns i textboxarna
+            string updateQuery = "UPDATE film SET Film = @Film, Regissör = @Regissör, Genre = @Genre, Rating = @Rating WHERE Film = @Film";
+            MySqlCommand cmd = new MySqlCommand(updateQuery, connection);
+
+            // Lägger till värdena i textboxarna som parametrar i SQL-frågan
+            cmd.Parameters.AddWithValue("@Film", filmBox1.Text);
+            cmd.Parameters.AddWithValue("@Regissör", regiBox3.Text);
+            cmd.Parameters.AddWithValue("@Genre", genreBox2.Text);
+            cmd.Parameters.AddWithValue("@Rating", ratingBox4.Text);
+
+            try
+            {
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data har uppdaterats!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+
+
+
 
         }
     }
